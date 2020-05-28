e3e70e93f3874b744eeb7e6acacd6b1096eea3bf
Claudio Castro <claudioluiz.castro@gmail.com>
2020-05-28 00:42:59 -0300

    delete Header component

--- src/__tests__/components/Header.spec.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Header from '../../components/Header';
-
-it('Render Header with title', () => {
-  const wrapper = shallow(<Header title="Be the hero" />);
-
-  console.log(wrapper);
-});
